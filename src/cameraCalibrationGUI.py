3071fb366bd8a698e584cb66b490d8f618ae6edd
Zachi Shtain <zachi.shtain@gmail.com>
2025-11-14 09:46:17 +0200

    update list of supported aruco dictionaries

--- src/cameraCalibrationGUI.py
+++ src/cameraCalibrationGUI.py
@@ -119,8 +119,10 @@ class CalibrationGui(QWidget):
         self.cmb_dict = QComboBox()
         # keep names that map to cv2.aruco attributes (we will getattr them later)
         self.cmb_dict.addItems([
-            "DICT_4X4_50", "DICT_4X4_100", "DICT_5X5_100",
-            "DICT_6X6_250", "DICT_7X7_1000"])
+            "DICT_4X4_50", "DICT_4X4_100", "DICT_4X4_250", "DICT_4X4_1000",
+            "DICT_5X5_50", "DICT_5X5_100", "DICT_5X5_250", "DICT_5X5_1000",
+            "DICT_6X6_50", "DICT_6X6_100", "DICT_6X6_250", "DICT_6X6_1000",
+            "DICT_7X7_50", "DICT_7X7_100", "DICT_7X7_250", "DICT_7X7_1000"])
 
         # Center both dropdown items and the currently displayed item
         delegate = CenteredComboBoxDelegate(self.cmb_dict)
